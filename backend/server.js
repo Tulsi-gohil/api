981a2140583240fccd3beebcb4518599cc0251f5
Tulsi-gohil <gohiltulsi451@gmail.com>
2026-02-16 17:59:34 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -196,7 +196,7 @@ app.post("/api/domain", async (req, res) => {
 
     app.post("/api/vahan", async (req, res) => {
       try {
-        const response = await client.get(
+        const response = await client.post(
           "https://vahan.parivahan.gov.in/nrservices/faces/user/citizen/citizenlogin.xhtml"
         );
 
